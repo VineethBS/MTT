38504c5d31b4a9c64c54de72b1793780a9fc00ae
vineeth <vineeth@iist>
2016-10-13 21:14:34 +0530

    Kalman filter implementation of predict and update

--- KalmanFilter.m
+++ KalmanFilter.m
@@ -1,8 +1,8 @@
 classdef KalmanFilter
     % Implementation of a Kalman filter for track prediction
     %   Kalman filter equations
-    %   x_{k + 1} = A x_{k} + w_{k}
-    %   y_{k} = C x_{k} + v_{k}
+    %   x_{k + 1} = A x_{k} + w_{k}; where x_{k} is the state
+    %   y_{k} = C x_{k} + v_{k}; where y_{k} is the observation
     %   Both w_{k} and v_{k} are jointly Gaussian vectors with zero mean
     %   and covariance matrices Q and Rs
     properties
@@ -10,9 +10,26 @@ classdef KalmanFilter
         C;
         Q;
         R;
+        state;
+        covariance;
+        predicted_state;
+        predicted_covariance;
+        kalman_gain;
     end
     
     methods
+        function [predicted_state, predicted_covariance] = predict(o)
+            o.predicted_state = o.A * o.state;
+            o.predicted_covariance = o.A * o.covariance * o.A' + o.Q;
+            predicted_state = o.predicted_state;
+            predicted_covariance = o.predicted_covariance;
+        end
+        
+        function update(o, observation)
+            o.kalman_gain = o.predicted_covariance * o.C' * inv(o.C * o.predicted_covariance * o.C' + o.R);
+            o.state = o.predicted_state + o.kalman_gain * (observation - o.C * o.predicted_state);
+            o.covariance = o.predicted_covariance - o.kalman_gain * o.C * o.predicted_covariance;
+        end
     end
     
 end
