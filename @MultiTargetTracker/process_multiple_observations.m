7623510ea40d9234fb9bedb4ca81e422ceb33a6d
vineeth <vineeth@iist>
2016-10-14 17:03:20 +0530

    Multi target tracker - added initial implementation for processing multiple observations

--- @MultiTargetTracker/process_multiple_observations.m
+++ @MultiTargetTracker/process_multiple_observations.m
@@ -1,3 +1,6 @@
 function o = process_multiple_observations(o, list_of_observations)
-            
+    num_list = length(list_of_observations);
+    for i = 1:num_list
+        o = o.process_one_observation(list_of_observations{i});
+    end
 end
