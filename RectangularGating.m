fc678bd08a0d83f8c6a84bbc39e1179c8903ddeb
vineeth <vineeth@iist>
2016-10-14 13:31:11 +0530

    Rectangular gating - Initial class definition for rectangular gating

---
+++ RectangularGating.m
@@ -0,0 +1,14 @@
+classdef RectangularGating
+    % Implementation of rectangular gating
+    
+    properties
+    end
+    
+    methods
+        function find_gate_membership(o, observations, list_of_tracks)
+            
+        end
+    end
+    
+end
+
