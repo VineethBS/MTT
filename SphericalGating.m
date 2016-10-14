bce457e0fc1ee1604676ea711e8a8f0ccb146071
vineeth <vineeth@iist>
2016-10-14 13:46:51 +0530

    Spherical gating 	1. Implementation of spherical gating 	2. Similar to rectangular gating, except the gating region is now spherical

---
+++ SphericalGating.m
@@ -0,0 +1,35 @@
+classdef SphericalGating
+    % Implementation of spherical gating
+    
+    properties
+        gate_radius;
+    end
+    
+    methods
+        function o = RectangularGating(gate_width)
+            o.gate_width = gate_width;
+        end
+        
+        function gate_membership_matrix = find_gate_membership(o, observations, list_of_tracks)
+            gate_membership_matrix = zeros(length(observations), length(list_of_tracks) + 1);
+            num_of_observations = length(observations);
+            num_of_tracks = length(list_of_tracks);
+            if num_of_observations > 0
+                for i = 1:num_of_observations
+                    current_observation = observations{i};
+                    for j = 1:num_of_tracks
+                        current_track = list_of_tracks{j};
+                        if distance(current_observation, current_track.get_observation()) <= o.gate_radius
+                            gate_membership_matrix(i, j) = 1;
+                        end
+                    end
+                    if sum(gate_membership_matrix(i, :)) == 0
+                        gate_membership_matrix(i, end) = 1;
+                    end
+                end     
+            end
+        end
+    end
+    
+end
+
