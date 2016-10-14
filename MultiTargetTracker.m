eea3eb6aac186e6cded026399e265fa5d05546f9
vineeth <vineeth@iist>
2016-10-14 14:49:28 +0530

    Multi target tracker 	1. Constructor added which can be used to set the type of filter, gating method, and data association method 	2. General cleanup in code formatting

--- MultiTargetTracker.m
+++ MultiTargetTracker.m
@@ -5,13 +5,38 @@ classdef MultiTargetTracker
         filter_type;
         gating_method_type;
         data_association_type;
+        
         gating;
         data_association;
-        list_of_tracks = [];
-        list_of_non_active_tracks = [];
+        
+        list_of_tracks;
+        list_of_non_active_tracks;
     end
     
     methods
+        function o = MultiTargetTracker(filter_type, gating_method_type, gating_method_parameters, data_association_type, data_association_parameters)
+            o.filter_type = filter_type;
+            o.gating_method_type = gating_method_type;
+            o.data_association_type = data_association_type;
+            
+            o.list_of_tracks = [];
+            o.list_of_non_active_tracks = [];
+            
+            if strcmp(o.data_association_type, 'GNN')
+                o.data_association = GlobalNearestNeighbour(data_association_parameters);
+            elseif strcmp(o.data_association_type, 'JPDA')
+                o.data_association = JProbDataAssociation(data_association_parameters);
+            elseif strcmp(o.data_association_type, 'Heuristic')
+                o.data_association = HeuristicDataAssociation(data_association_parameters);
+            end
+            
+            if strcmp(o.gating_method_type, 'Spherical')
+                o.gating = SphericalGating(gating_method_parameters);
+            elseif strcmp(o.gating_method_type, 'Rectangular')
+                o.gating = RectangularGating(gating_method_parameters);
+            end
+        end
+        
         function o = predict_new_positions(o)
             for i = 1:length(o.list_of_tracks)
                 o.list_of_tracks(i) = o.list_of_tracks(i).predict();
@@ -19,15 +44,11 @@ classdef MultiTargetTracker
         end
         
         function gate_membership_matrix = find_gate_membership(o, observations)
-            gate_membership_matrix = zeros(length(observations), length(o.list_of_tracks) + 1);
+            gate_membership_matrix = o.gating.find_gate_membership(observations, o.list_of_tracks);
         end
         
         function data_association_matrix = find_data_association(o, observations, gate_membership_matrix)
-            if strcmp(o.data_association_type, 'GNN')
-                data_association_matrix = o.data_association.find_data_association(observations, o.list_of_tracks);
-            elseif strcmp(o.data_association_type, 'JPDA')
-                data_association_matrix = o.data_association.find_data_association(observations, o.list_of_tracks, gate_membership_matrix);
-            end
+            data_association_matrix = o.data_association.find_data_association(observations, o.list_of_tracks, gate_membership_matrix);
         end
         
         function o = update_and_make_newtracks(o)
