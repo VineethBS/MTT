a0f74c86c3c88b5d01a32c7c6acab86322c6462f
vineeth <vineeth@iist>
2016-10-14 17:55:01 +0530

    MTT System - documentation updated

--- MTTSystem.m
+++ MTTSystem.m
@@ -1,12 +1,14 @@
 classdef MTTSystem
-    % Implementation of the complete MTT System
-        
+    % MTT system - models the complete MTT system consisting of the main
+    % MTT (tracker), the configuration (through a configuration file), and
+    % the data (through a data file).
+    
     properties
-        configuration_file;
-        data_file;
-        MTT;
-        dimension_observations;
-        num_of_observations;
+        configuration_file; % string
+        data_file; % string
+        MTT; % MTT 
+        dimension_observations; % int
+        num_of_observations; % int
     end
     
     methods
@@ -14,7 +16,7 @@ classdef MTTSystem
             o.configuration_file = configuration_file;
             o.data_file = data_file;
             
-            run(o.configuration_file);
+            run(o.configuration_file); % this will populate the local workspace with the configuration variables that we need
             
             o.MTT = MultiTargetTracker(filter_type, filter_parameters, gating_method_type, gating_method_parameters, ...
                                        data_association_type, data_association_parameters);
@@ -31,6 +33,7 @@ classdef MTTSystem
                     break;
                 end
                 tokens = strsplit(line, ',');
+                % convert all tokens to double
                 numeric_tokens = zeros(1, length(tokens));
                 for i = 1:length(tokens)
                     numeric_tokens(i) = str2double(tokens{i});
@@ -47,6 +50,8 @@ classdef MTTSystem
                 num_observations = floor((length(tokens) - 1)/o.dimension_observations);
                 observation_matrix = reshape(numeric_tokens, o.dimension_observations, num_observations);
                 
+                % the observations to be fed into MTT should be a
+                % cell-array
                 observations = {};
                 for i = 1:num_observations
                     observations{i} = observation_matrix(:, i);
