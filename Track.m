8c56dd1fe99b85a3a6a4adbdded5a8f763324bcb
vineeth <vineeth@iist>
2016-10-14 12:42:17 +0530

    Track - get predicted observation by doing C * state

--- Track.m
+++ Track.m
@@ -23,6 +23,10 @@ classdef Track
         function o = update(o, observation)
             o.kalman_filter = o.kalman_filter.update(observation);
         end
+        
+        function predicted_observation = get_observation(o)
+            predicted_observation = o.kalman_filter.get_observation();
+        end
     end
 end
 
