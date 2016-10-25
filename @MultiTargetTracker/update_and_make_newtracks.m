2b917d87eb073a82b45805f8d198ab67ef67ae73
vineeth <vineeth@iist>
2016-10-25 17:07:44 +0530

    Multi target tracker: splitting of a track added with proper initial conditions

--- @MultiTargetTracker/update_and_make_newtracks.m
+++ @MultiTargetTracker/update_and_make_newtracks.m
@@ -1,4 +1,4 @@
-function o = update_and_make_newtracks(o, time, observations, data_association_matrix)
+function o = update_and_make_newtracks(o, time, observations, gate_membership_matrix, data_association_matrix)
 
 new_tracks = {};
 num_of_observations = length(observations);
@@ -8,10 +8,26 @@ for i = 1:num_of_observations
     current_observation = observations{i};
 
     if data_association_matrix(i, end) == 1
-        initial_state = [current_observation', o.filter_parameters.rest_of_initial_state']';
-        t = Track(o.filter_parameters.A,  o.filter_parameters.C, o.filter_parameters.Q, o.filter_parameters.R, initial_state);
-        t = t.record_predicted_observation(time);
-        t = t.record_associated_observation(time, current_observation);
+        if gate_membership_matrix(i, end) == 1
+            initial_state = [current_observation', o.filter_parameters.rest_of_initial_state']';
+            t = Track(o.filter_parameters.A,  o.filter_parameters.C, o.filter_parameters.Q, o.filter_parameters.R, initial_state);
+            t = t.record_predicted_observation(time);
+            t = t.record_associated_observation(time, current_observation);
+        else
+            cost_vector = Inf * ones(1, num_of_tracks);
+            for j = 1:num_tracks
+                current_track = o.list_of_tracks{j};
+                if gate_membership_matrix(i, j) == 1
+                    cost_vector(j) = distance(current_observation, current_track.get_observation());
+                end
+            end
+            [~, track_from_which_split] = min(cost_vector);
+            t = o.list_of_tracks{track_from_which_split}.split_copy();
+            t = t.update(current_observation);
+            t = t.record_predicted_observation(time);
+            t = t.record_associated_observation(time, current_observation);
+        end
+
         new_tracks{end + 1} = t;
     else
         for j = 1:num_of_tracks
