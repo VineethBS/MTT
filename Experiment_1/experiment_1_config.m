a2ef541d1c5a1446b360e614d5c5f761d3664254
vineeth <vineeth@iist>
2016-11-03 16:26:05 +0530

    Experiment 1 - for testing of the code using heuristic data association

---
+++ Experiment_1/experiment_1_config.m
@@ -0,0 +1,39 @@
+% A sample configuration file
+
+dt = 1;
+
+dimension_observations = 1;
+num_of_observations = 99;
+field_separator = ',';
+
+filter_type = 'Kalman';
+
+filter_parameters.A = [1 dt dt^2
+                       0 1 dt
+                       0 0 1];
+                   
+filter_parameters.C = [1 0 0];
+
+filter_parameters.Q = [1 0 0;
+                       0 1 0;
+                       0 0 1];
+                   
+filter_parameters.R = 1;
+
+filter_parameters.rest_of_initial_state = [0
+                                           0];
+
+gating_method_type = 'Rectangular';
+gating_method_parameters.gate_width = 1;
+
+data_association_type = 'Heuristic';
+data_association_parameters.epsilon = 0.1;
+
+track_maintenance_type = 'NOutOfM';
+track_maintenance_parameters.N = 2;
+track_maintenance_parameters.M = 5;
+track_maintenance_parameters.confirm_threshold = 3;
+track_maintenance_parameters.confirm_M = 3;
+track_maintenance_parameters.confirm_N = 1;
+
+
