b8e7a6a7b7bfc603c1b13ad5d77d0312941bc4f1
vineeth <vineeth@iist>
2016-11-04 11:50:25 +0530

    Alpha Beta filter: implementation of alpha-beta filter

---
+++ AlphaBetaFilter.m
@@ -0,0 +1,49 @@
+classdef AlphaBetaFilter
+    % Implementation of a AlphaBeta filter for track prediction
+    %   AlphaBeta filter equations
+    %   x_{k + 1} = x_{k} + alpha * r_{k}; where x_{k} is the state
+    
+    %   y_{k} = x_{k}
+    %   Both w_{k} and v_{k} are jointly Gaussian vectors with zero mean
+    %   and covariance matrices Q and R
+    properties
+        A;
+        C;
+        alpha;
+        beta;
+        gain_observations;
+        gain_velocity;
+        state; % the current state
+        predicted_state;
+    end
+    
+    methods
+        % gobs and gvel are vectors with all ones of each with the dimension of observations
+        function o = AlphaBetaFilter(parameters, initial_observation)
+            o.A = parameters.A;
+            o.C = parameters.C;
+            o.gain_observations = parameters.gain_observations;
+            o.gain_velocity = parameters.gain_velocity;
+            o.alpha = parameters.alpha;
+            o.beta = parameters.beta;
+            initial_state = [initial_observation', parameters.rest_of_initial_state']';
+            o.state = initial_state;
+        end
+        
+        function o = predict(o)
+            o.predicted_state = o.A * o.state;
+        end
+        
+        function o = update(o, observation)
+            residual = observation - o.get_observation();
+            gain = [o.alpha * o.gain_observations; o.beta * o.gain_velocity] .* residual;
+            o.state = o.predicted_state + gain;          
+        end
+        
+        % return the observation corresponding to the current state of the filter
+        function predicted_observation = get_observation(o)
+            predicted_observation = o.C * o.state;
+        end
+    end
+end
+
