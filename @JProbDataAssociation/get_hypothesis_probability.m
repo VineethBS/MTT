33e73a4ac7ffccd22e4222b214eac540b856991d
vineeth <vineeth@iist>
2016-11-09 18:10:53 +0530

    JPDA - computation of hypothesis probability - probability of obs given hypothesis

--- @JProbDataAssociation/get_hypothesis_probability.m
+++ @JProbDataAssociation/get_hypothesis_probability.m
@@ -7,17 +7,21 @@ num_hypothesis = size(all_possible_valid_hypothesis, 2);
 num_tracks = length(list_of_tracks);
 
 hypothesis_probability = zeros(1, num_hypothesis);
+probability_of_obs_given_hypothesis = zeros(1, num_hypothesis);
+probability_of_hypothesis_given_past = zeros(1, num_hypothesis);
 for i = 1:num_hypothesis
     current_hypothesis = all_possible_valid_hypothesis(:, i);
-    hypothesis_probability(i) = 1;
+    probability_of_obs_given_hypothesis(i) = 1;
+    probability_of_hypothesis_given_past(i) = 1;
     for j = 1:num_observations
         current_observation = observations{j};
         if current_hypothesis(j) == 0
-            hypothesis_probability(i) = hypothesis_probability(i) * 1/o.volume;
+            probability_of_obs_given_hypothesis(i) = probability_of_obs_given_hypothesis(i) * 1/o.volume;
         else
             current_track = list_of_tracks{current_hypothesis(j)};
-            hypothesis_probability(i) = hypothesis_probability(i) * ... 
+            probability_of_obs_given_hypothesis(i) = probability_of_obs_given_hypothesis(i) * ... 
                 mvnpdf(current_observation, current_track.get_observation(), current_track.get_innovation_covariance);
         end
+    end
 end
 
