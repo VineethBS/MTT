4aabab2cd5c6c9f99c546d54a561f955f5364bac
vineeth <vineeth@iist>
2016-12-08 14:12:00 +0530

    UKF - general clean up of code formatting

--- UnscentedKalmanFilter.m
+++ UnscentedKalmanFilter.m
@@ -24,10 +24,12 @@ classdef UnscentedKalmanFilter
         state; % the current state
         covariance;
         predicted_state;
+        predicted_observation;
         predicted_covariance;
+        
         kalman_gain;
         cross_covariance;
-        measured_auto_covariance;
+        observation_covariance;
         
         alpha;
         ki;
@@ -56,7 +58,7 @@ classdef UnscentedKalmanFilter
             lambda = o.alpha^2 * (dimension_state + o.ki) - dimension_state;
             o.c = dimension_state + lambda;
             
-            o.Wm = [lambda/o.c, 0.5/c + zeros(1, 2 * dimension_state)];
+            o.Wm = [lambda/o.c, 0.5/o.c + zeros(1, 2 * dimension_state)];
             o.Wc = o.Wm;
             o.Wc(1) = o.Wc(1) + (1 - o.alpha^2 + o.beta);
             o.c = sqrt(o.c);
@@ -65,22 +67,24 @@ classdef UnscentedKalmanFilter
         function [y, transformed_sigmapoints, P, transformed_deviations] = ut(f, sigma_points, Wm, Wc, R)
             % Unscented Transformation
             % Input:
-            %        f: nonlinear map used for the transformation
-            %        sigma_points: sigma points used for the unscented transformation
+            %            f: nonlinear map used for the transformation
+            % sigma_points: sigma points used for the unscented transformation
             %       Wm: weights for mean
             %       Wc: weights for covariance
             %        R: additive covariance
             % Output:
             %        y: transformed mean
-            %        transformed_sigmapoints : transformed sampling points
+            % transformed_sigmapoints : transformed sampling points
             %        P: transformed covariance
             %       Y1: transformed deviations
             
             dimension_state = size(sigma_points, 1);
             number_sigmapoints = size(sigma_points, 2);
+            y = f(zeros(dimension_state, 1)); % find what dimension the output would have
+            dimension_output = length(y);
+            y = zeros(dimension_output, 1);
             
-            y = zeros(dimension_state, 1);
-            transformed_sigmapoints = zeros(dimension_state, number_sigmapoints);
+            transformed_sigmapoints = zeros(dimension_output, number_sigmapoints);
             
             for k=1:number_sigmapoints
                 transformed_sigmapoints(:,k) = f(sigma_points(:,k));
@@ -88,10 +92,10 @@ classdef UnscentedKalmanFilter
             end
             
             transformed_deviations = transformed_sigmapoints - y(:, ones(1, L));
-            P = transformed_deviations * diag(Wc) * transformed_deviations' +R;
+            P = transformed_deviations * diag(Wc) * transformed_deviations' + R;
         end
         
-        function sigma_points = sigmas(x, P, c)
+        function sigma_points = sigma_points(x, P, c)
             % Sigma points around reference point
             % Inputs:
             %       x: reference point
@@ -106,16 +110,16 @@ classdef UnscentedKalmanFilter
         end
         
         function o = predict(o)
-            sigma_points = o.sigmas(o.state, o.covariance, o.c);
+            sigma_points = o.sigma_points(o.state, o.covariance, o.c);
             [o.predicted_state, X1, o.predicted_covariance, X2] = o.ut(o.f, sigma_points, o.Wm, o.Wc, o.Q);
-            [o.predicted_observation, ~, o.measured_auto_covariance, Z2] = ut(o.h, X1, o.Wm, o.Wc, o.R);
+            [o.predicted_observation, ~, o.observation_covariance, Z2] = ut(o.h, X1, o.Wm, o.Wc, o.R);
             o.cross_covariance = X2 * diag(o.Wc) * Z2';
         end
         
         function o = update(o, observation)
-            o.kalman_gain=o.cross_covariance*inv(o.meas_auto_covariance);
-            o.state=o.predicted_state+o.kalman_gain*(observation-o.predicted_observation); 
-            o.covariance=o.predicted_covariance-o.kalman_gain*o.cross_covariance'; 
+            o.kalman_gain = o.cross_covariance * inv(o.observation_covariance);
+            o.state = o.predicted_state + o.kalman_gain*(observation-o.predicted_observation); 
+            o.covariance = o.predicted_covariance - o.kalman_gain * o.cross_covariance'; 
         end
         
         % return the observation corresponding to the current state of the filter
